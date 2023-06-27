8b7db7524be4ba900ed394948b314f54d316f17d
Aivenest <aivenest@gmail.com>
2023-06-27 19:53:33 +0500

    commit 3

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private static int count;
+
+    public Person(){
+        count++;
+    }
+
+    public static void getCount(){
+        System.out.println(count);
+    }
+}
